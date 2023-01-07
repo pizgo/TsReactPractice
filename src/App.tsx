c2c0a5e18e7fc1b4e9018f6991047c4d6ba969f4
Kasia Półrolniczak <k.polrolniczak@gmail.com>
2023-01-07 13:52:18 +0100

    handling useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './components/TodoList';
 import NewTodo from "./components/NewTodo";
+import {Todo} from './todo.model'
 
 const App: React.FC = () => {
-  const todos = [{ id: 't1', text: 'Finish the course' }];
+  const [todos, setTodos] = useState<Todo[]>([]); //tu się dzieje destrukturyzacja! use state zwraca tablicę z dwoma elementami(pierwszy to latest state snapshot, drugi to funkcja do update stan i rerender)  i tu od razu przypisujemy je do dwóch zmiennych
+    //precyzujemy jaki dokładnie typ danych będzie przekazywany do useState
 
   const todoAddHandler = (text: string) => {
-      
+    setTodos()
   }
   return (
     <div className="App">
-      <NewTodo/>
+      <NewTodo todoAddHandler={todoAddHandler}/> //przekazuję pointer do funkcji jako props
       <TodoList items={todos} />
     </div>
   );
